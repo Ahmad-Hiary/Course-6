cde2d22becd72488c313f28da6bc5b84a8bb3861
Ahmad-Hiary <p.ahmadhiary@gmail.com>
2025-09-20 13:44:04 +0300

    Add files via upload

---
+++ Two dimenstional arrays #27.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int c[10][10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            c[i][j] = (i + 1) * (j + 1);
+        }
+        
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%0*d ", 2, c[i][j]);
+        }
+        cout << endl;
+    }
+}
+
