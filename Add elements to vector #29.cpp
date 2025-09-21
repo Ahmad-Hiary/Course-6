f20a2cf1ce4f00afe9bde4dc6cfb77868b091e67
Ahmad-Hiary <p.ahmadhiary@gmail.com>
2025-09-21 12:03:51 +0300

    How to use vectors
    This code to practice vectors .

---
+++ Add elements to vector #29.cpp
@@ -0,0 +1,46 @@
+//practice on vectors .
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void ReadNumbers(vector <int> &vNumber)
+{
+	char Add = 'y';
+	int Number = 0;
+
+	while (Add == 'y' || Add == 'Y')
+	{
+		cout << "Please to add an element to vector enter a number ?\n";
+		cin >> Number;
+
+		vNumber.push_back(Number);
+
+		cout << "Do you want add an element to vector ?\n";
+		cin >> Add;
+	}
+}
+
+void PrintVectorElements(vector <int>& vNumber)
+{
+	cout << "Vectors elements are : ";
+
+	for (int Number : vNumber)
+	{
+		cout << Number << " ";
+	}
+}
+
+int main()
+{
+
+	vector <int> vNumbers;
+
+	ReadNumbers(vNumbers);
+	PrintVectorElements(vNumbers);
+	
+
+
+}
+
