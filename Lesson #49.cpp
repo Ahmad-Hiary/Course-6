0bfb1dd2383bc62ef3d28424c834af1dd381081b
Ahmad-Hiary <233337377+Ahmad-Hiary@users.noreply.github.com>
2025-09-25 01:40:38 +0300

    Exception handling .

---
+++ Lesson #49.cpp
@@ -0,0 +1,19 @@
+// ProgrammingAdvices.com © Copyright 2022
+// Lesson #49 – Code
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> num{ 1, 2, 3, 4, 5 };
+
+    try {
+        cout << num.at(5);  // ??? ????? ??? ??????? ??? ?????? ???? ??????
+    }
+    catch (...) {
+        cout << "out of bound\n";
+    }
+
+    return 0;
+}
